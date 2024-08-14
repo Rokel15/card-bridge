6fc03c8fcfed5d95d8b459efe9df77d8dd969398
Rokel15 <dab.loam15@gmail.com>
2024-08-14 10:30:28 +0700

    update

--- main.go
+++ main.go
@@ -40,15 +40,15 @@ func main() {
 	// 	host, port, user, password, dbName,
 	// )
 
-	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	// psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_PORT"),
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// )
 
-	// psqlInfo := os.Getenv("DATABASE_URL")
+	psqlInfo := os.Getenv("DATABASE_URL")
 
 	DB, err = sql.Open("postgres", psqlInfo)
 
